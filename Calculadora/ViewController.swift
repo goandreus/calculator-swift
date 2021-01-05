cbb2af37a92cef4ef66420c8d2b9a413373332b6
goandreus <andres.chavez.crisostomo@gmail.com>
2021-01-05 11:32:45 -0500

    view controller state (#1)

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -2,12 +2,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Propiedades
+    
+    @IBOutlet weak var resultadoLabel: UILabel!
+    
+    @IBOutlet weak var igualButton: UIButton!
+    var numeroAlmacenado: Int = 0
+    var operador: Operacion?
+    
+    //MARK: - Ciclo de vida de un ViewController
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultadoLabel.text = "0"
+        
     }
 
 
+    @IBAction func botonNumericoTapped(_ sender: UIButton) {
+        if resultadoLabel.text == "0" {
+            resultadoLabel.text = sender.titleLabel?.text
+        } else {
+          //  guard let unNumber = sender.titleLabel?.text{return}
+            let numero = resultadoLabel.text! + sender.titleLabel!.text!
+            resultadoLabel.text = numero
+            //resultadoLabel.text += sender.titleLabel?.text!
+        }
+    }
+    
+    
+    @IBAction func operacionButtonTapped(_ sender: UIButton) {
+        guard let numeroStr = resultadoLabel.text,
+              let numero = Int(numeroStr),
+              let simbolo = sender.titleLabel?.text
+              else {
+            return
+        }
+        operador = Operacion(rawValue: simbolo)
+        numeroAlmacenado = numero
+        resultadoLabel.text = "0"
+    }
+    
+    
+    @IBAction func igualButtonTapped(_ sender: UIButton) {
+        guard let operacion = operador,
+              let numeroStr = resultadoLabel.text,
+              let numero2 = Int(numeroStr)
+              else {
+                return
+              }
+        let resultado = operacion.operar(numeroA: numeroAlmacenado, numeroB: numero2)
+        resultadoLabel.text = "\(resultado)"
+    }
 }
 
