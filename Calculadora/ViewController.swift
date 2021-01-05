32f61a37ca47c0e6c216d19bbf17cb64983ae73e
goandreus <andres.chavez.crisostomo@gmail.com>
2021-01-05 15:53:22 -0500

    interface user and config json

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         resultadoLabel.text = "0"
+        igualButton.setImage(UIImage(systemName: "checkmark"), for: .normal)
         
     }
 
