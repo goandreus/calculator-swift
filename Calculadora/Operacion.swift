7f134374745aca2242fda8ea2811e5650a25267c
goandreus <andres.chavez.crisostomo@gmail.com>
2021-01-05 11:32:15 -0500

    add Operacion (#1)

--- Calculadora/Operacion.swift
+++ Calculadora/Operacion.swift
@@ -1,9 +1,31 @@
-//
-//  Operacion.swift
-//  Calculadora
-//
-//  Created by andres chavez on 1/5/21.
-//  Copyright © 2021 Academia Moviles. All rights reserved.
-//
 
 import Foundation
+
+enum Operacion: String {
+    case suma = "+"
+    case resta = "-"
+    case multiplicacion = "*"
+    case division = "/"
+    
+    func puedoOperar(numeroA: Int, numeroB: Int) -> Bool {
+        switch self {
+        case .division:
+            return numeroB != 0
+        default:
+            return true
+        }
+    }
+    
+    func operar(numeroA a:Int, numeroB b:Int) -> Int {
+        switch self  {
+        case .suma:
+            return a + b
+        case .resta:
+            return a - b
+        case .multiplicacion:
+            return a * b
+        case .division:
+            return a / b
+        }
+    }
+}
